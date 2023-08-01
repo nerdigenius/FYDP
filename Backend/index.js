fa43f48bb8383a3b1759fc911b4bd1b4fb4121d2
nerdigenius <Ashiq.racer@gmail.com>
2023-08-01 21:42:44 +0600

    init

---
+++ Backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = 8000;
+
+app.use(cors())
+app.get('/', (req, res) =>  res.json({ message: 'Hello, World!' }))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
