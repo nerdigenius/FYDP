6cc8c26e52a358362687d788247eb2fb2336f679
GingerAle <ssakib183013@bscse.uiu.ac.bd>
2023-09-22 16:21:29 +0600

    logout and connect button

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,29 +1,23 @@
 import './App.css'
 import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from './Login.js'
 import { Register } from './Register.js'
 import { Personal } from './Personal.js'
 import { Elections } from './Elections'
 
 function App() {
-
-  
-
   return (
     <div className="App">
-       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/signup" element={<Register />} />
-        <Route path='/personal' element={<Personal/>}/>
-        <Route path='/elections' element={<Elections/>}/>
-
       
-      </Routes>
-    </BrowserRouter>
-
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/elections" element={<Elections />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
