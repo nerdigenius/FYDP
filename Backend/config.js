3e65baeeeb891bacb03f9901a19618a2dffc6dbf
nerdigenius <Ashiq.racer@gmail.com>
2023-09-23 12:39:15 +0600

    Untrack files specified in .gitignore

--- Backend/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  PORT: process.env.PORT || 8000,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/devote',
-  JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
-};
--- FrontEnd/src/Ur_elections.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Ur_elections = () => {
-  return (
-    <div>
-      <h1>hello World</h1>
-    </div>
-  )
-}
