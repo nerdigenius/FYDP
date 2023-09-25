0d221a665b2acd45472a8707f45b5d983a1467d0
nerdigenius <Ashiq.racer@gmail.com>
2023-09-25 22:27:19 +0600

    routes protected

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,26 +1,32 @@
-import './App.css'
-import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Login } from './Login.js'
-import { Register } from './Register.js'
-import { Personal } from './Personal'
-import { Elections } from './Elections'
-import { Welcome } from './Welcome'
+import React from 'react';
+import { BrowserRouter, Routes, Route,Navigate  } from 'react-router-dom';
+import { Login } from './Login.js';
+import { Register } from './Register.js';
+import { Personal } from './Personal';
+import { Elections } from './Elections';
+import { Welcome } from './Welcome';
 
 function App() {
+  let isAuthenticated = localStorage.getItem('token');
+  console.log("is Authenticated: "+isAuthenticated)
+
   return (
-    <div className="App">      
+    <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
-          <Route path="/personal" element={<Personal />} />
-          <Route path="/elections" element={<Elections />} />
+          <Route path="/personal" element={isAuthenticated ? <Personal /> : <Navigate to="/login" />} />
+          <Route path="/elections" element={isAuthenticated ? <Elections /> : <Navigate to="/login" />} />
+        
+
+
+         
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
